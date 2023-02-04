00e63764353a115b03e89c17783dbf0d04143fad
karma8022 <neerajvarma2003@gmail.com>
2023-02-04 22:43:17 +0530

    Initial commit

--- ADS/Assignment 2/create_bt.cpp
+++ ADS/Assignment 2/create_bt.cpp
@@ -1,11 +1,93 @@
 #include <iostream>
-#include <stdlib.h>
 using namespace std;
 
-class Node{
-    public:
-        int data;
-        Node* left;
-        Node* right;
-        Node* root;
+class Treenode{
+    Treenode *left,*right;
+    int data;
+    friend class Tree;
 };
+
+class Tree {
+public:
+  Treenode *createTree();
+
+  void LVRrecursive(Treenode *root);
+  void LRVrecursive(Treenode *root);
+  void VLRrecursive(Treenode *root);
+};
+
+Treenode *Tree::createTree(){
+    Treenode *temp;
+    char choice;
+    int x;
+    Treenode *newnode = new Treenode;
+
+    cout<<"Enter data for node";
+    cin>>x;
+
+    if (x==-1){
+        return 0;
+    }
+
+    newnode->data =x;
+
+    cout<<"Enter left child"<<x<<endl;
+    newnode->left=createTree();
+
+    cout<<"Enter right child"<<x<<endl;
+    newnode->right=createTree();
+
+    return newnode;
+}
+
+void Tree::LRVrecursive(Treenode *root){
+    if (root==NULL){
+        return;
+    }
+    LRVrecursive(root->left);
+    LRVrecursive(root->right);
+    cout<<root->data<<" ";
+}
+
+void Tree::VLRrecursive(Treenode *root){
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    VLRrecursive(root->left);
+    VLRrecursive(root->right);
+}
+
+void Tree::LVRrecursive(Treenode *root){
+    if(root==NULL){
+        return;
+    }
+    LVRrecursive(root->left);
+    cout<<root->data<<" ";
+    LVRrecursive(root->right);
+}
+
+int main(int argc,char const *argv[]){
+    Tree t;
+    Treenode *finalroot = t.createTree();
+    cout<<"Post Order";
+    t.LRVrecursive(finalroot);
+    cout<<endl;
+    cout<<"In Order";
+    t.LVRrecursive(finalroot);
+    cout<<endl;
+    cout<<"Pre Order";
+    t.VLRrecursive(finalroot);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
